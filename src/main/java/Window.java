515bb2131a6e31dd256d96acf9cc97204aa48409
Udaya Sankar Kandukuru <kandukuruuday06@gmail.com>
2022-03-05 06:47:52 +0530

    Add files via upload

---
+++ src/main/java/Window.java
@@ -0,0 +1,17 @@
+public class Window {
+    private int Height;
+    private int Width;
+
+    public Window(int Width, int Height)
+    {
+        this.Width = Width;
+        this.Height = Height;
+    }
+    public int getHeight(){
+        return Height;
+    }
+
+    public int getWidth() {
+        return Width;
+    }
+}
